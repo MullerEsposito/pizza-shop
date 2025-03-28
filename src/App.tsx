bc39c08dd648d1c439ea49c414316551fbb90cfc
mulleresposito <mulleresposito@gmail.com>
2025-03-28 03:38:54 +0000

    initial commit;

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+import { Button } from './components/ui/button'
+
+export function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1>
+        Hello World!
+      </h1>
+      <Button>Send</Button>
+    </div>
+  )
+}
