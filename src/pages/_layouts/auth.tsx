0dd7b796c956e155f25dc50d87ac9307495ed1db
mulleresposito <mulleresposito@gmail.com>
2025-03-29 22:07:49 +0000

    style(AuthLayout):  -  adds the antialiased style to the AuthLayout container.  - adds the relative style to the AuthLayout wrapper.

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div className="grid min-h-screen grid-cols-2">
+    <div className="grid min-h-screen grid-cols-2 antialiased">
       <div className="border-r border-foreground/5 flex h-full flex-col justify-between p-10 bg-muted text-muted-foreground">
         <div className="flex items-center gap-3 text-lg text-foreground">
           <Pizza className="h-5 w-5" />
@@ -14,7 +14,7 @@ export function AuthLayout() {
           Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
         </footer>
       </div>
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center relative">
         <Outlet />
       </div>
     </div>
