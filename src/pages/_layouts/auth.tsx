d0ac17bc25976c13f17b1061b8e415b77fd9a68a
mulleresposito <mulleresposito@gmail.com>
2025-03-29 05:20:55 +0000

    feat(Layout):  - creates the App and Auth layouts.

---
+++ src/pages/_layouts/auth.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable @stylistic/max-len */
+import { Pizza } from 'lucide-react'
+import { Outlet } from 'react-router-dom'
+
+export function AuthLayout() {
+  return (
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="border-r border-foreground/5 flex h-full flex-col justify-between p-10 bg-muted text-muted-foreground">
+        <div className="flex items-center gap-3 text-lg text-foreground">
+          <Pizza className="h-5 w-5" />
+          <span className="font-semibold">pizza.shop</span>
+        </div>
+        <footer className="text-sm">
+          Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
+        </footer>
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <Outlet />
+      </div>
+    </div>
+  )
+}
