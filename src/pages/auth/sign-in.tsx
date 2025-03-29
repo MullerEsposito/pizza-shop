a46d83e18c7180c0b126742f1d918a5c06c6ea64
mulleresposito <mulleresposito@gmail.com>
2025-03-29 21:56:09 +0000

    feat(SignIn):  - handles sign in form with react-hook-form.  - creates new restaurant button with navigation.  - includes toaster component to notify user about success or error.  - uses zod to validate form data.

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,13 +1,49 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { useForm } from 'react-hook-form'
 import { SuperSEO } from 'react-super-seo'
+import { z } from 'zod'
+import { toast } from 'sonner'
+import { Link } from 'react-router-dom'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const signInForm = z.object({
+  email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>()
+
+  const handleSignInForm = async (data: SignInForm) => {
+    console.log(data)
+
+    try {
+      await new Promise(resolve => setTimeout(resolve, 2000))
+      toast.success('Enviaremos um link de autenticação para o seu e-mail!', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignInForm(data),
+        },
+      })
+    } catch {
+      toast.error('Credenciais inválidas.')
+    }
+  }
   return (
     <>
       <SuperSEO title="Auth | pizza.shop" />
       <div className="p-8">
+        <Button asChild variant="ghost" className="absolute top-8 right-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+
         <div className="flex flex-col w-[350px] justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
@@ -17,14 +53,14 @@ export function SignIn() {
               Acompanhe suas vendas pelo painel do parceiro
             </p>
           </div>
-          
-          <form className="space-y-4">
+
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignInForm)}>
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register('email')} />
             </div>
 
-            <Button className="w-full" type="submit">
+            <Button className="w-full" type="submit" disabled={isSubmitting}>
               Acessar painel
             </Button>
           </form>
