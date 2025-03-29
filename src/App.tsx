c70b339f81b91477161ef3acad1e76cc6eebab2e
mulleresposito <mulleresposito@gmail.com>
2025-03-29 22:05:51 +0000

    feat(Toaster):  - adds toaster provider to the app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import './index.css'
 
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 import { router } from './routes'
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Toaster richColors />
+    </>
+  )
 }
