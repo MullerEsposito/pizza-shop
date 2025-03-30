c159eb590e6bb0ffdc8011e5626e08b7840197dd
mulleresposito <mulleresposito@gmail.com>
2025-03-30 02:05:44 +0000

    feat(Theme):      - adds theme-provider from chadcn to apply toggle theme functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 import { router } from './routes'
+import { ThemeProvider } from './components/theme/theme-provider'
 
 export function App() {
   return (
-    <>
+    <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
       <RouterProvider router={router} />
       <Toaster richColors />
-    </>
+    </ThemeProvider>
   )
 }
