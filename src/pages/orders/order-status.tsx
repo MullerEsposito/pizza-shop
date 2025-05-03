c9401a0b675d604626da9c3cb0116c24a17ff3ca
mulleresposito <mulleresposito@gmail.com>
2025-05-03 22:05:46 +0000

    test(OrderStatus):  - creates tests to the following states of the component:    - pending    - processing    - delivering    - delivered    - canceled

--- src/pages/orders/order-status.tsx
+++ src/pages/orders/order-status.tsx
@@ -15,10 +15,14 @@ interface OrderStatusProps {
 export function OrderStatus({ status }: OrderStatusProps) {
     return (
         <div className="flex items-center gap-2">
-            {status === 'pending' && <span className="h-2 w-2 rounded-full bg-slate-400" />}
-            {status === 'canceled' && <span className="h-2 w-2 rounded-full bg-rose-500" />}
-            {status === 'delivering' && <span className="h-2 w-2 rounded-full bg-amber-500" />}
-            {['delivered', 'processing'].includes(status) && <span className="h-2 w-2 rounded-full bg-emerald-500" />}
+            {status === 'pending' && 
+                <span data-testid="badge" className="h-2 w-2 rounded-full bg-slate-400" />}
+            {status === 'canceled' && 
+                <span data-testid="badge" className="h-2 w-2 rounded-full bg-rose-500" />}
+            {status === 'delivering' && 
+                <span data-testid="badge" className="h-2 w-2 rounded-full bg-amber-500" />}
+            {['delivered', 'processing'].includes(status) && 
+                <span data-testid="badge" className="h-2 w-2 rounded-full bg-emerald-500" />}
             
             <span className="font-medium text-muted-foreground">
                 {orderStatusMap[status]}
